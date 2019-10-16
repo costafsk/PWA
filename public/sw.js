b08add9ccc754df815e3ae3bc22f8838770bd1ad
Diego Costa <diego.machado.d.costa@gmail.com>
2019-10-16 10:48:51 -0300

    teste

--- public/sw.js
+++ public/sw.js
@@ -22,16 +22,37 @@ self.addEventListener('install', function (event) {
     );
 });
 
-self.addEventListener('fetch', function (event) {
+
+// If any fetch fails, it will show the offline page.
+self.addEventListener("fetch", function (event) {
+    if (event.request.method !== "GET") return;
+    
     event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-            )
+        fetch(event.request).catch(function (error) {
+        // The following validates that the request was for a navigation to a new document
+        if (
+            event.request.destination !== "document" ||
+            event.request.mode !== "navigate"
+        ) {
+            return;
+        }
+  
+        console.error("[PWA Builder] Network request Failed. Serving offline page " + error);
+        return caches.open(CACHE).then(function (cache) {
+          return cache.match(offlineFallbackPage);
+        });
+      })
     );
-});  
+  });
+  
+  // This is an event that can be fired from your page to tell the SW to update the offline page
+  self.addEventListener("refreshOffline", function () {
+    const offlinePageRequest = new Request(offlineFallbackPage);
+  
+    return fetch(offlineFallbackPage).then(function (response) {
+      return caches.open(CACHE).then(function (cache) {
+        console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
+        return cache.put(offlinePageRequest, response);
+      });
+    });
+  });
